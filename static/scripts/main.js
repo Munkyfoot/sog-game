1c802c66074778cc099e09fdcf7c352b56df08bd
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2019-04-05 04:08:30 -0700

    Add id to tile divs

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,6 +4,6 @@ $(function(){
 
 $.getJSON('/json/', function(data){
     for(var i = 0; i < data.length; i++){
-        $('#gameboard').append("<div class='tile color_" + data[i]['color_id'] + "'></div>");
+        $('#gameboard').append("<div id='"+ data[i]['id'] + "' class='tile color_" + data[i]['color_id'] + "'></div>");
     }
 });
