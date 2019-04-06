77433901f3faae0330c59d00b1ac0dac82f452c4
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2019-04-06 03:12:05 -0700

    Adjust timing of AJAX request and change color of chat messages using ip

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,20 +1,20 @@
 var can_color = false;
 
-$(function(){
+$(function () {
     Generate();
 
-    setInterval(RefreshTiles, 1000);
+    setInterval(RefreshTiles, 2000);
 
-    $("#gameboard").on('click', '.tile', function(){
+    $("#gameboard").on('click', '.tile', function () {
         UpdateTile($(this).attr('id'));
     });
 
     RefreshMessages();
-    setInterval(RefreshMessages, 1500);
+    setInterval(RefreshMessages, 3000);
 
-    $("#chat_send").click(function(){
+    $("#chat_send").click(function () {
         var name = "Player"
-        if($("#chat_name").val().length > 0){
+        if ($("#chat_name").val().length > 0) {
             name = $("#chat_name").val();
         }
 
@@ -23,65 +23,75 @@ $(function(){
     });
 });
 
-function Generate(){
-    $.getJSON('/json/', function(data){
-        for(var i = 0; i < data.length; i++){
-            $('#gameboard').append("<div id='"+ data[i]['id'] + "' class='tile color_" + data[i]['color_id'] + "'></div>");
+function Generate() {
+    $.getJSON('/json/', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            $('#gameboard').append("<div id='" + data[i]['id'] + "' class='tile color_" + data[i]['color_id'] + "'></div>");
         }
     });
     $("#info").text("Feel free to change the color of a tile.");
     can_color = true;
 }
 
-function UpdateTile(tile_id){
-    if(!can_color){
+function UpdateTile(tile_id) {
+    if (!can_color) {
         return;
     }
 
-    $.post("/update/"+tile_id+"/").done(function( data ) {
-        for(var i = 0; i < data.length; i++){
-            $("#"+data[i]['id']).attr('class', 'tile color_' + data[i]['color_id'])
+    $.post("/update/" + tile_id + "/").done(function (data) {
+        for (var i = 0; i < data.length; i++) {
+            $("#" + data[i]['id']).attr('class', 'tile color_' + data[i]['color_id'])
         }
-
-        $("#info").text("Please wait a moment to recolor another tile.");
         can_color = false;
-        setTimeout(function(){
-            $("#info").text("You may now recolor another a tile.");
-            can_color = true;
-        }, 3000);
-    });
-}
 
-function RefreshTiles(){
-    $.getJSON('/json/', function(data){
-        for(var i = 0; i < data.length; i++){
-            $("#"+data[i]['id']).attr('class', 'tile color_' + data[i]['color_id'])
-        }
-    });
-}
-
-function SendMessage(name, message){
-    $.post("/message/", {name : name, message_content: message}).done(function( data ) {
-        $("#chat_output").html("Chat")
-        for(var i = 0; i < data.length; i++){
-            var name = data[i]['name'];
-            if(name == "Player"){
-                name += data[i]['ip_address'];
+        var s = 5;
+        $("#info").text("Please wait " + s + " seconds recolor another a tile.");
+        var x = setInterval(function () {
+            s--;
+            $("#info").text("Please wait " + s + " seconds recolor another a tile.");
+            if (s <= 0) {
+                $("#info").text("You may now recolor another a tile.");
+                can_color = true;
+                clearInterval(x);
             }
-            $("#chat_output").append("<div class='chat-message'>" + name + ": " + data[i]['content'] + "</div>")
+        }, 1000);
+    });
+}
+
+function RefreshTiles() {
+    $.getJSON('/json/', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            $("#" + data[i]['id']).attr('class', 'tile color_' + data[i]['color_id'])
         }
     });
 }
 
-function RefreshMessages(){
-    $.getJSON("/json/messages/").done(function( data ) {
+function SendMessage(name, message) {
+    $.post("/message/", { name: name, message_content: message }).done(function (data) {
         $("#chat_output").html("Chat")
-        for(var i = 0; i < data.length; i++){
-            var name = data[i]['name'];
-            if(name == "Player"){
-                name += data[i]['ip_address'].substring(0,3);
-            }
-            $("#chat_output").append("<div class='chat-message'>" + name + ": " + data[i]['content'] + "</div>")
+        for (var i = 0; i < data.length; i++) {
+            DisplayMessage(data[i]);
         }
     });
+}
+
+function RefreshMessages() {
+    $.getJSON("/json/messages/").done(function (data) {
+        $("#chat_output").html("Chat")
+        for (var i = 0; i < data.length; i++) {
+            DisplayMessage(data[i]);
+        }
+    });
+}
+
+function DisplayMessage(message_data) {
+    var name = message_data['name'];
+    if (name == "Player") {
+        name += message_data['ip_address'].substring(0, 3);
+    }
+
+    var ip_segments = message_data['ip_address'].split(".");
+    var color_style = "style='background-color:rgba(" + ip_segments[0] + ", " + ip_segments[1] + ", " + ip_segments[2] + ", 0.25);'";
+
+    $("#chat_output").append("<div class='chat-message' " + color_style + ">" + name + ": " + message_data['content'] + "</div>")
 }
